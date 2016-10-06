9f3dfd6342035115dd2900d01b3b17b7027ddc28
CJHwong <cjhwong@gmail.com>
2016-10-06 13:58:16 +0800

    First commit of a working new structure

--- urls.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
-import apps.entry
-urlpatterns = patterns('apps.entry.views',
-  url(r'^$','entry', name='entry'),#這樣做似乎是對應到首頁
-)
--- views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
-
-def entry(request):
-		return render_to_response('entry/entry.html',locals())
-
