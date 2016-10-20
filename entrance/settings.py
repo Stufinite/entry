ae1621498b9bea87d5456b799bee30f7a8231918
CJHwong <cjhwong@gmail.com>
2016-10-21 02:23:34 +0800

    Use usurper to fetch user info

--- entrance/settings.py
+++ entrance/settings.py
@@ -116,8 +116,12 @@ USE_TZ = True
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-from .settings_database import DATABASE_SETTINGS
-DATABASES = DATABASE_SETTINGS['sqlite']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
--- entry/views.py
+++ entry/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from userper import Userper
+u = Userper('login.stufinite.faith')
+
 
 def entry(request):
-    return render(request, 'entry/entry.html')
+    stufinite = {}
+    username = u.get_username()
+    if username != 'None':
+        stufinite['is_authenticated'] = True
+        stufinite['username'] = username
+    else:
+        stufinite['is_authenticated'] = False
+
+    return render(request, 'entry/entry.html', {'stufinite': stufinite})
